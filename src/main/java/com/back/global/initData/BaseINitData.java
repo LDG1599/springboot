0e6d337185c23008fac33512935b78b5d94c88cb
LDG1599 <yes388087@gmail.com>
2026-05-04 17:39:34 +0900

    sb5

--- src/main/java/com/back/global/initData/BaseINitData.java
+++ src/main/java/com/back/global/initData/BaseINitData.java
@@ -1,5 +1,6 @@
 package com.back.global.initData;
 
+import com.back.domain.post.post.entity.post;
 import com.back.domain.post.post.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
@@ -15,9 +16,21 @@ public class BaseINitData {
     ApplicationRunner baseInitDataApplicationRunner(){
 
         return args -> {
+            if (postRepository.count()>0) return;
+
+            post post1 =new post();
+            post1.setTitle("제목 1");
+
+            postRepository.save(post1);
+
+            post post2 =new post();
+            post2.setTitle("제목 2");
+
+            postRepository.save(post2);
+
             System.out.println("기본 데이터가 초기화되었습니다.");
 
-        postRepository.count();
+
         };
     }
 }
