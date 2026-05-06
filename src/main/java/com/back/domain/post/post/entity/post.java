391f33ab128c8b4c5b1c40fd241bd7770654edda
LDG1599 <yes388087@gmail.com>
2026-05-06 15:32:02 +0900

    sb7

--- src/main/java/com/back/domain/post/post/entity/post.java
+++ src/main/java/com/back/domain/post/post/entity/post.java
@@ -1,5 +1,6 @@
 package com.back.domain.post.post.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -17,5 +18,6 @@ public class post {
     @GeneratedValue(strategy = IDENTITY)
     private int id;
     private String title;
+    @Column(columnDefinition = "TEXT")
     private String content;
 }
