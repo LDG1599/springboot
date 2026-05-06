853ac6ca1e6b0b765560d6d8cf3758c6fed83b8e
LDG1599 <yes388087@gmail.com>
2026-05-06 17:40:58 +0900

    sb14

--- src/main/java/com/back/domain/post/post/service/postService.java
+++ src/main/java/com/back/domain/post/post/service/postService.java
@@ -3,10 +3,10 @@ package com.back.domain.post.post.service;
 import com.back.domain.post.post.entity.post;
 import com.back.domain.post.post.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import java.util.Optional;
 
-@Component
+@Service
 
 public class postService {
     @Autowired
