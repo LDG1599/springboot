fcf47c15514224fe31c559ae03b6251f10375cc0
LDG1599 <yes388087@gmail.com>
2026-05-06 17:16:56 +0900

    sb12

--- src/main/java/com/back/domain/post/post/entity/post.java
+++ src/main/java/com/back/domain/post/post/entity/post.java
@@ -9,6 +9,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
@@ -22,11 +23,15 @@ public class post {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private int id;
+    private LocalDateTime createDate;
+    private LocalDateTime modifyDate;
     private String title;
     @Column(columnDefinition = "TEXT")
     private String content;
 
     public post(String title, String content) {
+        this.createDate = LocalDateTime.now();
+        this.modifyDate = this.createDate;
         this.title = title;
         this.content = content;
     }
